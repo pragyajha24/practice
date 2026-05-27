ef327dbff30f5a5a8fdece9a971ac57d083a187a
Pragya Jha <pragyajha50@gmail.com>
2026-05-28 00:19:43 +0530

    practiced usePopcorn code

---
+++ usepopcorn/src/main.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <App /> */}
+
+    <StarRating maxRating={5} size={24} color="red" className="test" />
+    <StarRating
+      defaultRating={3}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+
+    <Test />
+  </React.StrictMode>,
+);
